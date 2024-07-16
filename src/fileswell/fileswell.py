87e0520b5cff991f76659d0515adeaeb80678df4
Jannis Ahlers <jannis.ahlers@monash.edu>
2024-07-16 18:00:08 +1000

    Add some docstrings

--- src/fileswell/fileswell.py
+++ src/fileswell/fileswell.py
@@ -16,7 +16,21 @@ from roifile import ImagejRoi
 
 
 class ROI(object):
+    """A region-of-interest (ROI) defined by a set of coordinates."""
     def __init__(self, coords):
+        """Constructs a ROI
+
+        Parameters
+        ----------
+        coords : ndarray
+            An Nx2 array of coordinates defining the ROI, with the first column
+            being the y-coordinates and the second column being the x-coordinates.
+
+        Returns
+        -------
+        ROI
+            A ROI object.
+        """
         self.global_coords = coords
 
         self.top = round(np.min(coords[:, 0]))
@@ -46,11 +60,24 @@ class ROI(object):
 
     @classmethod
     def from_imagej_roi(cls, imagej_roi: ImagejRoi):
+        """Create a ROI from an ImagejRoi object."""
         coords = np.roll(imagej_roi.coordinates(), 1, axis=1)
         return cls(coords)
 
 
 def load_imagej_rois(path: Path):
+    """Load ImageJ ROIs from a .roi or .zip (containing .roi files) file.
+
+    Parameters
+    ----------
+    path : Path
+        The path to the .roi or .zip file.
+
+    Returns
+    -------
+    list of ROI
+        A list of ROI objects.
+    """
     rois = []
     if path.name.endswith('.zip'):
         with ZipFile(path, "r") as zf:
@@ -67,6 +94,57 @@ def load_imagej_rois(path: Path):
 
 
 def extract_line_profile(im, edgewidth=5, linelength=10, linewidth=3, roi=None, ax=None):
+    """Extract a line profile along an edge in an image.
+
+    This function extracts an averaged line profile along an edge in an image.
+    It was first designed to extract line profiles from edges in CT slices of
+    fairly homogenous materials. If there is a lot of noise, or the materials are
+    very inhomogenous, it may not work so well.
+
+    The function works by segmenting the images into two regions, one with a high
+    intensity and one with a low intensity. It then finds the edge between these
+    two regions using a Canny edge detector. At each point on the edge it takes
+    a line profile perpendicular to the edge, and then averages these line profiles.
+
+    Therefore, you need to supply a region-of-interest (ROI) that contains the edge
+    you want to extract the line profile from, and some of the two regions sorrounding
+    that edge, but no other features.
+
+    If the image already contains nothing more than the edge and the two regions, you
+    don't need to supply a ROI.
+
+    Parameters
+    ----------
+    im : ndarray
+        The image to extract the line profile from.
+    edgewidth : int, optional (default=5)
+        The estimated width of the edge, in pixels.
+        For example, in a propagation-based phase-contrast CT slice, this would be the
+        width of the phase fringes. Does not need to be exact.
+    linelength : int, optional (default=10)
+        The length of the line profile either side of the edge, in pixels.
+    linewidth : int, optional (default=3)
+        The width of each line profile, in pixels.
+    roi : ROI, optional (default=None)
+        The region-of-interest containing the edge and the two regions surrounding it.
+        If None, the ROI is the whole image.
+    ax : array of matplotlib axes, optional (default=None)
+        Needs to be a list or array of two matplotlib axes. The first axis will be used
+        to show an image of the section of the image containing the edge and the line,
+        with a sample of line profiles drawn on top. The second axis will be used to
+        plot the line profiles. If None, no plots will be made.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the following:
+        - line_profile : array
+            The line profile along the edge, as a numpy array of uncertainties.
+        - intensity_high : ufloat
+            The mean intensity (and std) in the high intensity region.
+        - intensity_low : ufloat
+            The mean intensity (and std) in the low intensity region.
+    """
 
     # If no region-of-interest is provided, the ROI is the whole image
     if roi is None:
